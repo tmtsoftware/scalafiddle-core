4f4b864a2be29e77d2311cd391de71b1155ddcd9
Otto Chrons <otto@chrons.me>
2017-12-07 00:07:52 +0200

    Gitbook integration

--- integrations/gitbook/index.js
+++ integrations/gitbook/index.js
@@ -1,9 +1,70 @@
+const fs = require("fs");
+const path = require("path");
+const cfg = {
+  "templateDir": {
+    "type": "string",
+    "default": "templates",
+    "description": "Location of ScalaFiddle template files"
+  },
+  "scalaFiddleUrl": {
+    "type": "string",
+    "default": "https://embed.scalafiddle.io/",
+    "description": "ScalaFiddle service URL"
+  }
+};
+
+const defaultOptions = Object.freeze({
+  templateDir: cfg.templateDir.default,
+  scalaFiddleUrl: cfg.scalaFiddleUrl.default
+});
+
 const validParams = ["template", "minheight", "layout", "theme"];
+
 const processArgs = args => {
   const params = {};
   validParams.forEach(p => {if (args[p]) params[p] = args[p]});
   return params
-}
+};
+
+const convertValue = (valstr, valtype) => {
+  if (valtype === "boolean" || valtype === "number") {
+    return JSON.parse(valstr);
+  }
+  return valstr;
+};
+
+const initOptions = (options) => {
+  const kv = Object.assign({}, defaultOptions);
+  // Overwrite default value with user book options.
+  Object.keys(defaultOptions).forEach(key => {
+    if (options.config.get(key) !== undefined) {
+      kv[key] = convertValue(options.config.get(key), typeof defaultOptions[key]);
+    }
+  });
+  return Object.freeze(kv);
+};
+
+const escapeStr = str => str.replace(/\r/g,"").replace(/\t/g,"\\t").replace(/'/g,"\\\'")
+
+const readTemplate = file => {
+  const lines = fs.readFileSync(file, "utf-8").split("\n");
+  const pre = [];
+  const post = [];
+  let inPre = true;
+  lines.forEach(line => {
+    if (line.startsWith("////")) {
+      inPre = false
+    } else if (inPre) {
+      pre.push(line)
+    } else {
+      post.push(line)
+    }
+  });
+  return {
+    pre,
+    post
+  }
+};
 
 module.exports = {
   blocks: {
@@ -22,7 +83,10 @@ module.exports = {
   hooks: {
     page: function (page) {
       const content = page.content;
+      const book = this;
+      const options = initOptions(this);
       const scalaFiddleRE = /<div data-scalafiddle/g;
+
       if (content.match(scalaFiddleRE)) {
         const templateRE = /<div data-scalafiddle data-template="([^"]+)"/g;
         const templates = {};
@@ -33,11 +97,22 @@ module.exports = {
           m = templateRE.exec(content);
         }
         if (Object.keys(templates).length > 0) {
-          const templateDefs = Object.keys(templates).map(key => `    "${key}": {\n      pre: "",\n      post: ""\n    }`).join(",\n");
+          const templateDefs = Object.keys(templates).map(function(key) {
+            const fileName = book.resolve(`${options.templateDir}/${key}.scala`);
+            const tf = readTemplate(fileName);
+            return `    '${key}': {\n      pre: '${tf.pre.map(escapeStr).join("\\n")}',\n      post: '${tf.post.map(escapeStr).join("\\n")}'\n    }`
+          }).join(",\n");
           script += `<script>\n  window.scalaFiddleTemplates = {\n${templateDefs}\n}</script>\n`
         }
-        script += `<script src="https://embed.scalafiddle.io/integration.js"></script>`
+        script += `<script src="${options.scalaFiddleUrl}integration.js"></script>\n`
         console.log(script)
+        // insert script just before body-end tag, if it exists
+        const bodyIdx = page.content.lastIndexOf("</body>")
+        if(bodyIdx > 0) {
+          page.content = page.content.slice(0, bodyIdx).concat(script, page.content.slice(bodyIdx));
+        } else {
+          page.content = page.content.concat(script);
+        }
       }
       return page
     }
--- integrations/gitbook/test/index.test.js
+++ integrations/gitbook/test/index.test.js
@@ -5,6 +5,8 @@ const examples = require('./examples')
 const setupBook = content => {
   return tester
     .builder()
+    .withFile("templates/custom.scala", "import custom\nobject test {\n////\n}\n")
+    .withFile("templates/another.scala", "import another")
     .withContent(content)
     .withLocalPlugin(pluginDir)
     .create()
@@ -13,26 +15,18 @@ const setupBook = content => {
 describe('Injecting ScalaFiddle integration', () => {
   jest.setTimeout(20000)
 
-/*
-  it('should return HTML with ScalaFiddle div injected', done => {
-    setupBook(examples.plain)
-      .then(function (results) {
-        console.log(results[0].content)
-        expect(results[0].content.includes("data-scalafiddle")).toBe(true)
-      })
-      .then(done)
-      .done()
-  })
-*/
-
   it('should return HTML with ScalaFiddle attributes in place', done => {
     setupBook(examples.params)
       .then(function (results) {
-        console.log(results[0].content)
-        expect(results[0].content.includes("data-scalafiddle")).toBe(true)
-        expect(results[0].content.includes("data-template=\"custom\"")).toBe(true)
-        expect(results[0].content.includes("data-layout=\"v70\"")).toBe(true)
-        expect(results[0].content.includes("data-theme=\"dark\"")).toBe(true)
+        console.log(results[0].content);
+        expect(results[0].content.includes("data-scalafiddle")).toBe(true);
+        expect(results[0].content.includes("data-template=\"custom\"")).toBe(true);
+        expect(results[0].content.includes("data-layout=\"v70\"")).toBe(true);
+        expect(results[0].content.includes("data-theme=\"dark\"")).toBe(true);
+
+        expect(results[0].content.includes(`pre: 'import custom\\nobject test {',`)).toBe(true);
+        expect(results[0].content.includes(`post: ''`)).toBe(true);
+        expect(results[0].content.includes(`post: '}\\n'`)).toBe(true);
       })
       .then(done)
       .done()
